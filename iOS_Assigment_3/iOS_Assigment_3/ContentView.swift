7361f3380c7360c8ad489d6c31c1ff5ee7b16b00
Ky.iOS <p.kystaal@gmail.com>
2024-04-23 15:26:22 +1000

    Added assignment project

---
+++ iOS_Assigment_3/iOS_Assigment_3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS_Assigment_3
+//
+//  Created by Ky Staal on 23/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_Assigment_3/iOS_Assigment_3/iOS_Assignment_3App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Assignment_3App.swift
+//  iOS_Assignment_3
+//
+//  Created by Ky Staal on 23/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Assignment_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
